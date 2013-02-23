75618d51365875871537007428c6b6e9b6c739b9
William Martin <will.st4@gmail.com>
2013-02-23 20:34:49 +0000

    Initialised project.

---
+++ src/org/jds/mds/MDS.java
@@ -0,0 +1,11 @@
+package org.jds.mds;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: will
+ * Date: 23/02/13
+ * Time: 20:32
+ * To change this template use File | Settings | File Templates.
+ */
+public class MDS {
+}
